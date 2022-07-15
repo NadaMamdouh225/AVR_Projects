73354c1e5231466e1c1dbe80f908a20bcfffe85f
NadaMamdouh225 <nada20011002@gmail.com>
2022-07-15 17:53:03 +0200

    added key map feature to KPD driver

--- Drivers/IMT_School/COTS/HAL/KPD/KPD_int.h
+++ Drivers/IMT_School/COTS/HAL/KPD/KPD_int.h
@@ -10,9 +10,22 @@
 
 
 #define KPD_PORT    DIO_PORTB
+#define ROWS          4
+#define COLS          4
+
+/*
+ * 	u8 KeyMap[ROWS][COLS] = {   {'1','2','3','A' },
+					            {'4','5','6','B' },
+				                {'7','8','9','C' },
+					            {'*','0','#','D' }
+			                                             };
+
+ *
+ */
 
 
 void KPD_vInit(void);
-u8 KPD_u8GetPressedKey(void);
+u8 KPD_u8GetPressedKey(u8 A_u8KeyMap[ROWS][COLS]);
+
 
 #endif /* IMT_SCHOOL_COTS_HAL_KPD_KPD_INT_H_ */
--- Drivers/IMT_School/COTS/HAL/KPD/KPD_prg.c
+++ Drivers/IMT_School/COTS/HAL/KPD/KPD_prg.c
@@ -4,19 +4,30 @@
  *  Created on: Jul 2, 2022
  *      Author: Nada Mamdouh
  */
+
 #include "../../LIB/STD_Types.h"
 #include "../../MCAL/DIO/DIO_int.h"
-#include "KPD_int.h"
 #include <util/delay.h>
-
+#include "KPD_int.h"
 
 void KPD_vInit(void)
 {
 	DIO_vSetPortDir(KPD_PORT, 0x0f);
 	DIO_vSetPortVal(KPD_PORT, 0xff);
 }
-u8 KPD_u8GetPressedKey(void)
+
+u8 KPD_u8GetPressedKey(u8 A_u8KeyMap[ROWS][COLS])
 {
+
+	for(u8 L_u8IDX_I = 0;L_u8IDX_I < ROWS ;L_u8IDX_I++ )
+	{
+		for(u8 L_u8IDX_J = 0;L_u8IDX_J < COLS ;L_u8IDX_J++ )
+		{
+			A_u8KeyMap[L_u8IDX_I][L_u8IDX_J] ;
+		}
+	}
+
+
 	u8 pressed_key = 0;  // default value if nothing pressed
 	for(u8 L_u8Col=0; L_u8Col<4 ;L_u8Col++)
 	{
@@ -27,7 +38,7 @@ u8 KPD_u8GetPressedKey(void)
 		{
 			if(DIO_u8GetPinVal(KPD_PORT,L_u8Row+4) == 0)  // a button is pressed
 			{
-				pressed_key = L_u8Row + (L_u8Col*4) +1;  // key number
+				pressed_key = A_u8KeyMap[L_u8Row][L_u8Col];  // key number   L_u8Row + (L_u8Col*4) +1
 			}
 			while(DIO_u8GetPinVal(KPD_PORT,L_u8Row+4) == 0){}
 			_delay_ms(10);
